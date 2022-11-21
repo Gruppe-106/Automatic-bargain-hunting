9b6878c7a9202957f5ea5ef7e9eacb58991032b9
Andreas-Hum <hummelmosea@gmail.com>
2022-11-21 12:58:05 +0100

    swag

--- test.c
+++ test.c
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-void main(int) {
-    int shit = 1122313123213231312321321321321222221;
-=======
 int main(void) {
     int test = 1;
     int test2 = 2;
     int test5 = 4;
     int shit = 1122313123213222221;
-
->>>>>>> c03a86b72863ab5e81928bd27235485b1f021664
+    int dswwapåwoasdsakdad = 231;
     return 0;
 }
