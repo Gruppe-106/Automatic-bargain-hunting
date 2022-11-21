ec5614a6eea526cc5cae0d840b49cb0c21898c76
Andreas-Hum <hummelmosea@gmail.com>
2022-11-21 13:00:35 +0100

    Shit lord

--- test.c
+++ test.c
@@ -2,7 +2,7 @@ int main(void) {
     int test = 1;
     int test2 = 2;
     int test5 = 4;
-    int shit = 1122313123213222221;
+    int shit = 112231312321322222122222;
     int dswwapåwoasdsakdad = 231;
     return 0;
 }
