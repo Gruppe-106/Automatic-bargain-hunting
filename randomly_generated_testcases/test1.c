e780d2b80c95b48402301e18f28d896086bb83dd
madsbyriel <madshbyriel@gmail.com>
2022-11-24 12:11:52 +0100

    RANDOM TESTCASES IS DONE! Generates unsorted store lists and generates sorted store lists.

--- randomly_generated_testcases/test1.c
+++ randomly_generated_testcases/test1.c
@@ -1,13 +1,11 @@
 #include "../randomly_generated_testcases/random_testcases.h"
-#include "../randomly_generated_testcases/random_testcases.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include "time.h"
 
 int main(void)
 {
-    Store_Type* store = generate_stores_unsorted(5, 5);
+    Store_Type* store = generate_stores_unsorted(20, 20);
     print_all_stores_test(store);
     free_all_stores_test(store);
-
 }
