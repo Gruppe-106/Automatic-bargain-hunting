dfe1f9f4d6d7c5a0cc501e15eaaa7a9ad89bdba4
Christianbonde <cbje22@student.aau.dk>
2022-11-25 12:09:11 +0100

    Prøver nu om det virker

--- src/data_manager/sort_stores.c
+++ src/data_manager/sort_stores.c
@@ -1,7 +1,29 @@
 #include "sort_stores.h"
+#include <stdlib.h>
+
+int compare_price_of_items(const void *p, const void *q);
 
 /**
  * Sorts the list of items for one store using qsort()
  * @param store Store_Type ptr, ptr to store node needing to be sorted
  */
-void sort_store_items(Store_Type *store);
+Store_Type *sort_store_items(Store_Type *store)
+{
+    Item_Type** itemsSorted = store->items;
+    qsort(store->items, store->item_amount, sizeof(Item_Type*), compare_price_of_items);
+    store->items = itemsSorted;
+
+    return store;
+}
+
+int compare_price_of_items(const void *p, const void *q)
+{
+    Item_Type* item1 = *(Item_Type**)p;
+    Item_Type* item2 = *(Item_Type**)q;
+    if (item1->price < item2->price)
+        return -1;
+    else if (item1->price > item2->price)
+        return 1;
+    else
+        return 0;
+}
--- src/data_manager/sort_stores.h
+++ src/data_manager/sort_stores.h
@@ -2,5 +2,5 @@
 #define AUTOMATIC_BARGAIN_HUNTING_SORT_STORES_H
 #include "../data_manager/data_manager.h"
 
-void sort_store_items(Store_Type *store);
+Store_Type *sort_store_items(Store_Type *store);
 #endif //AUTOMATIC_BARGAIN_HUNTING_SORT_STORES_H
