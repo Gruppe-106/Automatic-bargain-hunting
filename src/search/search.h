851e0fde0de0b99ecb233580c849c03c8320bf4d
madsbyriel <madshbyriel@gmail.com>
2022-11-28 12:22:59 +0100

    Fixed search and testcases.

--- src/search/search.h
+++ src/search/search.h
@@ -1,6 +1,6 @@
 #ifndef AUTOMATIC_BARGAIN_HUNTING_SEARCH_H
 #define AUTOMATIC_BARGAIN_HUNTING_SEARCH_H
 #include "../data_manager/data_manager.h"
-Item_Type** find_items_in_items_by_str(char* query, Item_Type** items, int* size_output);
+Item_Type** find_items_in_items_by_str(char* query, Item_Type** items, size_t len, int* size_output);
 
 #endif //AUTOMATIC_BARGAIN_HUNTING_SEARCH_H
