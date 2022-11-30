98cc8e12e3532795ee03de859eb4e1634fd680d3
Andreas-Hum <heyoallos@gmail.com>
2022-11-30 18:46:38 +0100

    prototype done

--- src/io/user_io.c
+++ src/io/user_io.c
@@ -1,46 +1,104 @@
 /* Standard libraries */
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 /* Custom libraries */
 #include "user_io.h"
 #include "../data_manager/data_manager.h"
+#include "../util/user_io_utility.h"
+#include "../util/user_io_utility.c"
 
 /* Prototype */
+
+
+Input_Item* create_new_input_item(char* name){
+
+    Input_Item* new_item = (Input_Item*) malloc(sizeof(Input_Item));
+    new_item->next_input = NULL;
+    char *item_name      = (char*) malloc(sizeof(char)*(strlen(name)+1));
+
+    str_to_lower(&name);
+    memcpy(new_item->input, name, strlen(name) + 1);
+
+    return new_item;
+}
 
 /**
  * @brief Append an Input_Item to list
  * @param list Input_Item ptr, ptr to list to append too
  * @param input char*, the given input from user
  */
-void append_input_item_node(Input_Item* list, char* input);
+void append_input_item_node(Input_Item** input_item, char* input){
+    if((*input_item)->next_input == NULL){
+        (*input_item)->next_input = create_new_input_item(input);
+        return;
+    } else {
+        append_input_item_node((*input_item)->next_input, input);
+    }
+};
 
 /**
  * @brief Gets user input using format\n
  * User input format: "something" enter "something" enter .... done enter
  * @return Input_Item ptr, linked list of all inputs
  */
-Input_Item* user_input(Input_Item* item_list, char* item_input)
+void user_driver()
 {
-    /* we start by allocating space for a new item. The item already contains enough space for the item. */
-    Input_Item* new_item = (Input_Item*) malloc(sizeof(Input_Item));
+    _Bool flag = false;
+    char* name;
+    Item_Type *grocery_list;
 
-    /* If not allocated space exit */
-    if(new_item == NULL)
-        exit(EXIT_FAILURE);
+    while(!flag){
+        scanf  ("%s", &name);
+        tolower(&name);
 
-    /* we use strcpy here because C does not support direct assignment. */
-    strcpy(new_item->input, item_input);
+        if( strcmp(name, "find") == 0 ){
+            /* CALL MAIN FUNCTION HERE */
+            break;
+        }else
+        if( strcmp(name, "quit") == 0 ) {
+            /* FREE THINGS */
+            break;
+        } else {
+
+            if(grocery_list == NULL){
+                grocery_list = create_new_input_item(name);
+            } else {
+                append_input_item_node(&grocery_list, name);
+            }
+
+        }
+    }
 }
 
-/**
- * @brief deallocate, allocated memory from list
- * @param list Input_Item ptr, ptr to list to free
- */
-void deallocate_user_input(Input_Item* list);
 
-/**
- * @brief List stores in order of cheapest to most expensive
- * @param all_stores Store_Type prt, ptr to the list of all stores
- */
-void user_output(Store_Type* all_stores);
+void free_grocery_list(Input_Item* grocery_list){
+    int length = grocery_list_length(grocery_list);
+
+    Input_Item* next    = grocery_list->next_input;
+    Input_Item* current = grocery_list;
+
+    for (int i = 0; i < length; i++)
+    {
+        free(current->input);
+        free(current);
+        current = next;
+        next = next->next_input;
+    }
+
+}
+
+
+// /**
+//  * @brief deallocate, allocated memory from list
+//  * @param list Input_Item ptr, ptr to list to free
+//  */
+// void deallocate_user_input(Input_Item* list);
+
+// /**
+//  * @brief List stores in order of cheapest to most expensive
+//  * @param all_stores Store_Type prt, ptr to the list of all stores
+//  */
+// void user_output(Store_Type* all_stores);
