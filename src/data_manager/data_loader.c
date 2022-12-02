de03091dee7bbd491bca7a394f5d1d0d5e07fc55
Oragif <Mikkelhelsing1@gmail.com>
2022-12-02 13:02:14 +0100

    Added data_loader.c

---
+++ src/data_manager/data_loader.c
@@ -0,0 +1,17 @@
+#include "data_loader.h"
+
+#define BILKA_PATH "data/bilka.json"
+#define COOP_PATH "data/coop.json"
+#define FOETEX_PATH "data/foetex.json"
+#define REMA_PATH "data/rema-1000.json"
+
+Store_Type** load_all_data() {
+    Store_Type **all_stores = NULL;
+
+    update_stores(json_parse_file(BILKA_PATH), all_stores, "Bilka");
+    update_stores(json_parse_file(COOP_PATH), all_stores, "Coop");
+    update_stores(json_parse_file(FOETEX_PATH), all_stores, "Foetex");
+    update_stores(json_parse_file(REMA_PATH), all_stores, "Rema-1000");
+
+    return all_stores;
+}
