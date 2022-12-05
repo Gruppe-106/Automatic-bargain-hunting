c91d9a758fbdcc7c741687f2931095d84950790b
Mikkel <miande22@student.aau.dk>
2022-12-05 09:46:17 +0100

    Fixed data_loader.c

--- src/data_manager/data_loader.c
+++ src/data_manager/data_loader.c
@@ -5,13 +5,13 @@
 #define FOETEX_PATH "data/foetex.json"
 #define REMA_PATH "data/rema-1000.json"
 
-Store_Type** load_all_data() {
-    Store_Type **all_stores = NULL;
+Store_Type* load_all_data() {
+    Store_Type *all_stores = NULL;
 
-    update_stores(json_parse_file(BILKA_PATH), all_stores, "Bilka");
-    update_stores(json_parse_file(COOP_PATH), all_stores, "Coop");
-    update_stores(json_parse_file(FOETEX_PATH), all_stores, "Foetex");
-    update_stores(json_parse_file(REMA_PATH), all_stores, "Rema-1000");
+    update_stores(json_parse_file(BILKA_PATH), &all_stores, "Bilka");
+    update_stores(json_parse_file(COOP_PATH), &all_stores, "Coop");
+    update_stores(json_parse_file(FOETEX_PATH), &all_stores, "Foetex");
+    update_stores(json_parse_file(REMA_PATH), &all_stores, "Rema-1000");
 
     return all_stores;
 }
--- src/data_manager/data_loader.h
+++ src/data_manager/data_loader.h
@@ -2,5 +2,5 @@
 #define AUTOMATIC_BARGAIN_HUNTING_DATA_LOADER_H
 #include "data_manager.h"
 
-Store_Type** load_all_data();
+Store_Type* load_all_data();
 #endif //AUTOMATIC_BARGAIN_HUNTING_DATA_LOADER_H
