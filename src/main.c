e80a47a01468965d1369dcf457e434a698bc1c65
madsbyriel <madshbyriel@gmail.com>
2022-12-08 14:46:55 +0100

    Nice sorting algorithm that wasnt hard to make at all, who even reads these comments hello?

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "data_manager/rank_stores.h"
 #include "util/user_io_utility.h"
 #include "util/node_handler.h"
+#include <stdio.h>
 
 int main(void) {
     Store_Type *all_stores = load_all_data();
@@ -14,9 +15,16 @@ int main(void) {
     size_t length;
     Store_Result_Type **results = search_stores(items_to_find, all_stores, &length);
 
+
     print_results(results, length);
+    sort_results(&results, length);
+    for (int i = 0; i < length; ++i) {
+        printf("STORE: %s\n", results[i]->store);
+        printf("Total Grocery List Price: %lf\n", results[i]->price_of_groceries);
+        printf("Total Missing Items: %d\n\n", results[i]->missing_items);
+    }
+
     free_results(results, length);
     free_stores(all_stores);
-
     return 0;
 }
