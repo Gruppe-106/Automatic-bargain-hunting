606d87ae21736a8fa71262caf6662da0e77a6741
Andreas <ahumme22@student.aau.dk>
2022-12-13 10:13:18 +0100

    Test for user done

--- tests/io/user_io.c
+++ tests/io/user_io.c
@@ -9,36 +9,38 @@
 int main(void){
     Input_Item *test_case = input_grocery_list(true);
     Input_Item *current = test_case;
+    _Bool flag = true;
     int length = grocery_list_length(test_case);
 
     puts("User_io test");
 
 
     if(length != 10){
-        free_grocery_list(test_case);
         printf("Expected a length of 10 got %d",length);
-        return EXIT_FAILURE;
-    }
+        flag = false;
+    } else {
+        for (int i = 0; i < 10; ++i) {
+            if(strcmp(current->input,"test") != 0){
+                printf("Expected input to be test got %s",current->input);
+                flag = false;
+                break;
 
-    for (int i = 0; i < 10; ++i) {
-        if(strcmp(current->input,"test") != 0){
-            printf("Expected input to be test got %s",current->input);
-            free_grocery_list(test_case);
-            return EXIT_FAILURE;
-        } else if(current->quantity != 1){
-            printf("Expected a quantity of 1 got %d",current->quantity);
-            free_grocery_list(test_case);
-            return EXIT_FAILURE;
-        } else {
-            printf("test case %d: Expected input to be test got %s\n",i+1,current->input);
-            printf("test case %d: Expected a quantity of 1 got %d\n", i+1, current->quantity);
-            current = current->next_input;
+            } else if(current->quantity != 1){
+                printf("Expected a quantity of 1 got %d",current->quantity);
+                flag = false;
+                break;
+            } else {
+                printf("test case %d: Expected input to be test got %s, expected a quantity of 1 got %d\n",i+1,current->input,current->quantity);
+                current = current->next_input;
+            }
         }
     }
 
     free_grocery_list(test_case);
-    return EXIT_SUCCESS;
-
-
+    if(!flag){
+        return EXIT_FAILURE;
+    } else {
+        return EXIT_SUCCESS;
+    }
 
 }
